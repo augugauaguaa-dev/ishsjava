df00fec8ca626a146c615de2ae1b4e5744994789
unknown <augugauaguaa@gmail.com>
2025-09-11 19:35:15 +0900

    v0/6 PrimeGame

--- week05/src/PrimeGame_2.java
+++ week05/src/PrimeGame_2.java
@@ -1,12 +1,11 @@
-import java.util.Scanner;
-
 public class PrimeGame_2 {
-    public static boolean isPrime(int n) {
-        if (n <= 1) {
+
+    public static boolean isPrime(int n){
+        if(n <= 1){
             return false;
-        } else {
-            for (int i = 2; i <= Math.sqrt(n); i = i + 1) {
-                if (n % i == 0) {
+        }else{
+            for(int i=2; i<=Math.sqrt(n); i=i+1){
+                if(n % i == 0){
                     return false;
                 }
             }
@@ -15,16 +14,15 @@ public class PrimeGame_2 {
     }
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int number = sc.nextInt();
+        int[] numbers = {111, 977, 1, 2, 53, 1200};  // 배열
 
+        for(int i=0; i<6; i++){
+            if(isPrime(numbers[i])){
+                System.out.println(numbers[i] + " is a prime number~");
+            }else{
+                System.out.println(numbers[i] + " is NOT prime number!");
+            }
 
-        if (isPrime(number)) {
-            System.out.println(number + " is prime number");
-        } else {
-            System.out.println(number + " is not prime number");
         }
-
-
     }
 }
