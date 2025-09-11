b2562b39323d7c66d2bfd30107fe27a6cdac6615
unknown <augugauaguaa@gmail.com>
2025-09-11 19:38:21 +0900

    v0.7

--- week05/src/PrimeGame_2.java
+++ week05/src/PrimeGame_2.java
@@ -16,13 +16,22 @@ public class PrimeGame_2 {
     public static void main(String[] args) {
         int[] numbers = {111, 977, 1, 2, 53, 1200};  // 배열
 
-        for(int i=0; i<6; i++){
-            if(isPrime(numbers[i])){
-                System.out.println(numbers[i] + " is a prime number~");
+
+        for(int number : numbers){//범위기반 for문 ranged based for
+            if(isPrime(number)){
+                System.out.println(number + " is a prime number~");
             }else{
-                System.out.println(numbers[i] + " is NOT prime number!");
+                System.out.println(number + " is NOT prime number!");
             }
 
         }
+//        for(int i=0; i<6; i++){
+//            if(isPrime(numbers[i])){
+//                System.out.println(numbers[i] + " is a prime number~");
+//            }else{
+//                System.out.println(numbers[i] + " is NOT prime number!");
+//            }
+//
+//        }
     }
 }
