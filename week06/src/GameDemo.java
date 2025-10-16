0249654f94a5b39fc3343ad9a6702e652644845e
unknown <augugauaguaa@gmail.com>
2025-10-16 19:54:24 +0900

    v0.2 Pokemon Game

--- week06/src/GameDemo.java
+++ week06/src/GameDemo.java
@@ -1,5 +1,13 @@
 public class GameDemo {
     public static void main(String[] args) {
-    System.out.println("Game Start~");
+        Pikachu mypikachu = new Pikachu(100);
+        Pikachu yourpikachu = new Pikachu(100);
+        System.out.println("Game Start~");
+        mypikachu.attack(yourpikachu);
+
+        Squirtle mysquirtle = new Squirtle(100);
+        Squirtle yoursquirtle = new Squirtle(100);
+        System.out.println("Game Start~");
+        mysquirtle.attack(yoursquirtle);
     }
 }
