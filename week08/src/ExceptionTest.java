0d5b5434713a04734ef7e67d27c702ecead82224
unknown <augugauaguaa@gmail.com>
2025-10-30 21:08:16 +0900

    exveption handling v0.4

--- week08/src/ExceptionTest.java
+++ week08/src/ExceptionTest.java
@@ -7,18 +7,28 @@ public class ExceptionTest {
        int numerator = 0;
        int denominator = 0;
 
-
        try {
            System.out.print("Input numerator : ");
            numerator = scanner.nextInt();
            System.out.print("Input denominator : ");
            denominator = scanner.nextInt();
+           int[] array = {99, -7, 33};
 
            System.out.println(numerator/denominator);
        }catch (InputMismatchException err){
             System.out.println("숫자로 입력하세요.");
-       }catch (ArithmeticException err){
+            System.out.println(err.getMessage());
+       }catch (ArithmeticException err) {
            System.out.println("분모에 0이 올 수 없습니다!");
+           System.out.println(err.getMessage());
+
+       }catch (ArrayIndexOutOfBoundsException err){
+           System.out.println("인덱스의 범위를 벗어났습니다.");
+           System.out.println(err.getMessage());
+       }catch (Exception err){
+           System.out.println("에러 발생");
+       }finally {
+           System.out.println("프로그램을 종료합니다");
        }
     }
 }
