6c3d1897015a5cdf6cb5e79daf745a118f60e8dc
unknown <augugauaguaa@gmail.com>
2025-10-30 21:42:36 +0900

    v0.6 Pokemon Game

--- week08/src/GameDemo.java
+++ week08/src/GameDemo.java
@@ -1,3 +1,10 @@
+import fly.NoFly;
+import fly.Wings;
+import pokemons.Charizard;
+import pokemons.Pikachu;
+import pokemons.Pokemon;
+import pokemons.Squirtle;
+
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
@@ -42,23 +49,23 @@ public class GameDemo {
         else if (randomNumber == 2)
             enemyPokemon = new Charizard(200, 40, new Wings());
 
-//        Pokemon playerPokemon = new Pikachu(100, 27, new NoFly());
-//        Pokemon playerPokemon = new Charizard(200, 40, new Wings());
+//        pokemons.Pokemon playerPokemon = new pokemons.Pikachu(100, 27, new fly.NoFly());
+//        pokemons.Pokemon playerPokemon = new pokemons.Charizard(200, 40, new fly.Wings());
 //
 //
-//        Jetpack jetpack = new Jetpack();
+//        fly.Jetpack jetpack = new fly.Jetpack();
 //
 //
-////        Pikachu p1 = new Pikachu(100, 27, new NoFly());
-////        //p1.setFlyingTool(new NoFly());
+////        pokemons.Pikachu p1 = new pokemons.Pikachu(100, 27, new fly.NoFly());
+////        //p1.setFlyingTool(new fly.NoFly());
 ////        p1.performFlyable();
 ////        p1.setFlyingTool(jetpack);
 ////        p1.performFlyable();
 ////
-////        Charizard c1 = new Charizard(200, 40, new Wings());
-////        //c1.setFlyingTool(new Wings());
+////        pokemons.Charizard c1 = new pokemons.Charizard(200, 40, new fly.Wings());
+////        //c1.setFlyingTool(new fly.Wings());
 ////        c1.performFlyable();
-////        // Squirtle s1 = new Squirtle(120, 21);
+////        // pokemons.Squirtle s1 = new pokemons.Squirtle(120, 21);
 //
 //        System.out.println("배틀 시작!");
 //        System.out.println(p1);
@@ -83,9 +90,9 @@ public class GameDemo {
 //            turn++;
 //        }
 
-//        Pikachu mypikachu = new Pikachu(100, 27);
-//        Pikachu yourpikachu = new Pikachu(100, 28);
-//        Squirtle enemysquirtle = new Squirtle(120, 21);
+//        pokemons.Pikachu mypikachu = new pokemons.Pikachu(100, 27);
+//        pokemons.Pikachu yourpikachu = new pokemons.Pikachu(100, 28);
+//        pokemons.Squirtle enemysquirtle = new pokemons.Squirtle(120, 21);
 //        System.out.println("Game Start~");
 //        mypikachu.attack(enemysquirtle);
 //
