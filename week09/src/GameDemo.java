a1aeae817f79cdda55c5edab24d0582b35b0b0e9
unknown <augugauaguaa@gmail.com>
2025-11-06 20:34:48 +0900

    v1.0 Pokemongame

--- week09/src/GameDemo.java
+++ week09/src/GameDemo.java
@@ -7,8 +7,6 @@ import java.util.Scanner;
 
 public class GameDemo {
     public static void main(String[] args) {
-//        int randomNumber = (int)(Math.random() * 3);
-//        System.out.println(randomNumber);
         Skill[] pikachuSkills = {
                 new Skill("Nuzzle", 20),
                 new Skill("ThunderShock", 40),
@@ -54,7 +52,7 @@ public class GameDemo {
             }
 
         int randomNumber = (int)(Math.random() * 3);
-        Pokemon enemyPokemon;
+        Pokemon enemyPokemon = null;
         System.out.println("야생의 포켓몬스터가 나타났습니다");
         if (randomNumber == 0)
             enemyPokemon = new Pikachu(100, 27, new NoFly(), pikachuSkills);
@@ -70,6 +68,13 @@ public class GameDemo {
         for(int i=0; i<playerPokemon.skills.length; i++){
             System.out.println((i+1) + ". " + playerPokemon.skills[i].getName() + " (" + playerPokemon.skills[i].getDamage() + ")");
         }
+
+        System.out.print("Select skill : ");
+        int skillNumber = scanner.nextInt() - 1;
+
+        enemyPokemon.setHp(enemyPokemon.getHp()-playerPokemon.skills[skillNumber].getDamage());
+        System.out.print(enemyPokemon.getName() + "의 체력이" + enemyPokemon.getHp() + "남았습니다");
+
 //        int turn = 1;
 //        while (!p1.isFained() && !c1.isFained()){
 //            System.out.println("턴 " + turn + " 시작.");
